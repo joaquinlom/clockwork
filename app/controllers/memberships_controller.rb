8c1e1577addc528ccd2fde3a934f371f4231676e
Luis Antonio Ramirez Martinez <luis190991@gmail.com>
2013-11-05 19:09:27 -0700

    Cambios generales a la administración de usuarios, y corrección de tags de Imagenes.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,17 +1,37 @@
 class MembershipsController < ApplicationController
   
 	def index
-   id_user =  session[:user_id]
-   id_membership = params[:id]
+    id_user =  session[:user_id]
+    id_membership = params[:id]
    
-   if id_user == nil || id_membership == nil
-     render "welcome/index"
-   else
-     user = User.find(id_user)
-     @membership =  user.memberships.find(id_membership)
-     @memberships = Membership.find(:all, :conditions =>["organization_id <= ? ",@membership.organization.id] )
-     @users = User.all()
-   end
+    if id_user == nil || id_membership == nil
+      render "welcome/index"
+    else
+      user = User.find(id_user)
+      @membership =  user.memberships.find(id_membership)
+      @memberships = Membership.find(:all, :conditions =>["organization_id <= ? ",@membership.organization.id] )
+      @users = User.all()
+      @roles = Rol.all()
+    end
 	end
+  
+  def create
+    id_role = params[:role_id]
+    id_organization = params[:organization_id]
+    id_user = params[:user_id]
+    
+    user = User.find(id_user)
+    rol = Rol.find(id_role)
+    organization = Organization.find(id_organization)
+    
+    membership = user.memberships.new()
+    membership.rol = rol
+    membership.organization = organization
+    
+  end
+  
+  def author_params
+    params.require(:memebership).permit(:organization_id, :role_id, :user_id)
+  end
   
 end
