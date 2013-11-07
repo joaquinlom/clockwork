ce5f3e697c4adb9536659b6cc0b29d558539f182
Fernando Gomez <dgomezpayan@gmail.com>
2013-11-07 18:42:23 +0000

    creacion de un db:seed para ejemplos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ 
+
+Rol.create(name: 'Administrador', description: 'Cambios en general')
+Rol.create(name: 'Miembro', description: 'Usuario del sistema')
+
+Organization.create(name: 'Proteccion de animales', description: 'Proteger a los animasles')
+Organization.create(name: 'Amigos de los animales', description: 'Asociacion distinguida por los beneficios que ofrece a los animalitos')
+
+Membership.create(organization_id: 1, rol_id: 1, user_id: 1)
+
+Schedule.create(job:'Director', membership_id: 1)
+
+Schedule_detail.create(entry_time: Date.today, exit_time: Date.today+1, week_day: 'Lunes', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+1, exit_time: Date.today+2, week_day: 'Martes', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+2, exit_time: Date.today+3, week_day: 'Miercoles', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+3, exit_time: Date.today+4, week_day: 'Jueves', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+4, exit_time: Date.today+5, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+5, exit_time: Date.today+6, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: 1)
+Schedule_detail.create(entry_time: Date.today+6, exit_time: Date.today+7, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: 1)
