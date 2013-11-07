5fdab5a08b28ea65dd350b78cddc1d5c6ecd2793
Fernando Gomez <dgomezpayan@gmail.com>
2013-11-07 21:25:50 +0000

    Se creo un seed con ejemplos, visualizar para ver contenido, necesario el user_id=1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
  
 
+#User.create(user_name: 'Fernando Gomez Payan')
+
 Rol.create(name: 'Administrador', description: 'Cambios en general')
 Rol.create(name: 'Miembro', description: 'Usuario del sistema')
 
@@ -16,11 +18,23 @@ Organization.create(name: 'Amigos de los animales', description: 'Asociacion dis
 Membership.create(organization_id: 1, rol_id: 1, user_id: 1)
 
 Schedule.create(job:'Director', membership_id: 1)
+Schedule.create(job:'Gerente', membership_id: 1)
 
-Schedule_detail.create(entry_time: Date.today, exit_time: Date.today+1, week_day: 'Lunes', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+1, exit_time: Date.today+2, week_day: 'Martes', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+2, exit_time: Date.today+3, week_day: 'Miercoles', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+3, exit_time: Date.today+4, week_day: 'Jueves', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+4, exit_time: Date.today+5, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+5, exit_time: Date.today+6, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: 1)
-Schedule_detail.create(entry_time: Date.today+6, exit_time: Date.today+7, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: 1)
+s = Schedule.find(1)
+s1 = Schedule.find(2)
+
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Lunes', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Martes', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Miercoles', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Jueves', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: s)
+s.schedule_details.create(entry_time: 1, exit_time: 2, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: s)
+
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Lunes', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Martes', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Miercoles', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Jueves', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: s1)
+s.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: s1)
