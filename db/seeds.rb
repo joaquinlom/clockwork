ce719ae9c2e8e83dfc79a06cd250679e2319cdae
Fernando Gomez <dgomezpayan@gmail.com>
2013-11-09 01:30:48 +0000

    se agregaron mas ejemplos al seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,7 @@ Membership.create(organization_id: 2, rol_id: 2, user_id: 1)
 
 Schedule.create(job:'Director', membership_id: 1)
 Schedule.create(job:'Gerente', membership_id: 1)
+Schedule.create(job:'silvestre', membership_id: 1)
 
 s = Schedule.find(1)
 
@@ -40,4 +41,14 @@ sc.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Jueves', lati
 sc.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: sc)
 sc.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: sc)
 sc.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: sc)
+
+sc1 = Schedule.find(3)
+
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Lunes', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Martes', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Miercoles', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Jueves', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Viernes', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Sabado', latitude: 1, length: 2, schedule_id: sc1)
+sc1.schedule_details.create(entry_time: 3, exit_time: 4, week_day: 'Domingo', latitude: 1, length: 2, schedule_id: sc1)
 
