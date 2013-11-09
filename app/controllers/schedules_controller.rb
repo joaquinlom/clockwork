a05b6d9bd4f29b8ed61215cfe5e8c03faeabecd6
Fernando Gomez <dgomezpayan@gmail.com>
2013-11-09 01:29:07 +0000

    se modifico controlador

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,4 +7,13 @@ class SchedulesController < ApplicationController
     user = User.find(id_user)
     @membership =  user.memberships.find(id_membership)
 	end
+  
+  def new
+    user = User.find(id_user)
+  end
+  
+  def author_params
+    params.require(:schedule).permit(:job, :membership_id)
+  end
+  
 end
