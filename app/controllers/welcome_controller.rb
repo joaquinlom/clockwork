753f2103171b2414affb3a6ed397aba3b944ceeb
Luis Antonio Ramirez Martinez <luis190991@gmail.com>
2013-11-10 10:07:12 -0700

    Corrección de errores con los filtros

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     if session[:user_id] == nil
       render()
     else
-      render 'organizations/index'
+      redirect_to organizations_path
     end
 	end
 end
