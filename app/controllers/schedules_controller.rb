2d0913891e4e44911af360c1ef9be305296f730f
Fernando Gomez <dgomezpayan@gmail.com>
2013-11-14 21:26:43 +0000

    Modificacion del controlador schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,12 +1,17 @@
 class SchedulesController < ApplicationController
   
-	def index
+  before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+  
+  def index
     id_user =  session[:user_id]
     id_membership = params[:id]
     
     user = User.find(id_user)
     @membership =  user.memberships.find(id_membership)
 	end
+  
+  def show
+  end
   
   def new
     id_user =  session[:user_id]
@@ -25,7 +30,35 @@ class SchedulesController < ApplicationController
       render :action => "new"
     end
   end
-
+  
+  def edit
+  end
+  
+  def update
+    if @schedule.update_attributes(shcedule_params)
+      flash[:notice] = 'Cambios Guardados Correctamente'
+      redirect_to :action => 'index'
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @schedule.destroy
+    redirect_to :action => 'index'
+  end
+  
+  private
+  
+  def set_schedule
+    id_user =  session[:user_id]
+    id_membership = params[:id]
+    
+    user = User.find(id_user)
+    membership =  user.memberships.find(id_membership)
+    @schedule = membership.schedules.find(params[:id])
+  end
+  
   def schedule_params
     params.require(:schedule).permit(:job, :membership_id)
   end
