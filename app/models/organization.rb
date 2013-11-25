c0ad0492939f90596fdaa36aa3a0d1e6dfd6c15b
Alejandro Chaires <chaires.a@gmail.com>
2013-11-25 16:08:46 -0700

    Validaciones a las Organizaciones

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,7 @@
 class Organization < ActiveRecord::Base
+	validates :description,:presence => true
+  validates :name,:presence => true
+  validates :name, :length => {:minimum => 4}
 	has_many :memberships
 	has_many :users, :through => :memberships
 end
